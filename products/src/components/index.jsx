e5fb0aed752cf6b06fa1cea2f3a1e6af80e335d8
Awais881 <alizayn881@gmail.com>
2022-12-12 22:38:50 +0500

    completed

--- products/src/components/index.jsx
+++ products/src/components/index.jsx
@@ -52,7 +52,7 @@ function Products() {
         try {
           const response = await axios.get(`${baseUrl}/products`)
           console.log("response: ", response.data);
-          setProducts(response.data.products.reverse())
+          setProducts(response.data.data.reverse());
     
     
         } catch (error) {
